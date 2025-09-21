3866fc2d1e5e70035c1222284fdfbb630b4535b4
Nathan <bignate5464@gmail.com>
2025-09-21 17:16:23 -0400

    initial commit

---
+++ src/TgaContainer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bigna on 9/21/2025.
+//
+
+#include "TgaContainer.hpp"
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by bigna on 9/21/2025.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
